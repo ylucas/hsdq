35667d03edfe239691e5413e4e83047ab4c2285d
Yves Lucas <ylucas@ylucas.com>
2013-04-21 00:35:51 -0700

    setting

---
+++ lib/hsdq/setting.rb
@@ -0,0 +1,59 @@
+
+
+module Hsdq
+  module Setting
+    def hsdq_opts(opts={})
+      @hsdqopts ||= read_opts.merge opts
+    end
+
+    def default_opts
+      @default_opts ||= {
+        :threaded => false,
+        :timeout  => 60
+      }
+    end
+
+    def read_opts(file_path=nil)
+      begin
+        default_opts.merge YAML.load_file(file_path || config_file_path)[:"#{environment}"]
+      rescue
+        puts "[warning] config file not read, using default options"
+        default_opts
+      end
+    end
+
+    def environment(environment=nil)
+      environment  ||= (defined?(Rails) ? Rails.env : nil) || RAILS_ENV || 'development'
+      @environment ||= environment
+    end
+
+    def snakify(string)
+      string.split(/(?=[A-Z])/).map(&:downcase).join('_')
+    end
+
+    def channel(name=nil)
+      @channel ||= name || snakify(hsdq_get_name.gsub(/^hsdq/i, ""))
+    end
+
+    def hsdq_get_name
+      self.respond_to?(:name) ? self.name : self.class.name
+    end
+
+    def channel=(name)
+      @channel = name
+    end
+
+    def config_filename(filename=nil)
+      @config_filename ||= filename || "#{snakify(hsdq_get_name.gsub(/^hsdq/i, ""))}.yml"
+    end
+
+    def config_path(path=nil)
+      @config_file_path ||= path || "#{(defined?(Rails) ? Rails.root : '.')}/config/"
+    end
+
+    def config_file_path(config_file_path=nil)
+      @config_file_path ||= config_file_path || File.join(config_path, config_filename)
+    end
+
+  end
+end
---
+++ spec/lib/hsdq/setting_spec.rb
@@ -0,0 +1,74 @@
+require_relative '../../shared/hsdq_shared_setup'
+
+RSpec.describe Hsdq::Setting do
+  include_context "setup_shared"
+
+  describe "#default_opts" do
+    it { expect( obj.default_opts).to eq ({:threaded => false, :timeout  => 60}) }
+  end
+
+  describe "#hsdq_opts" do
+    it { expect( obj.hsdq_opts({:threaded=>true})).to eq ({:threaded => true, :timeout  => 60}) }
+  end
+
+  describe "#environmemt" do
+    context "set by RAILS_ENV" do
+      it { expect(obj.environment).to eq 'test' }
+    end
+
+    context "set by parameter" do
+      it { expect(obj.environment("development")).to eq 'development' }
+    end
+  end
+
+  describe "#snakify" do
+    it { expect(obj.snakify("MyGoodClass")).to eq 'my_good_class' }
+  end
+
+  describe "#channel" do
+    context "passed param" do
+      it { expect(obj.channel('my_channel')).to eq 'my_channel' }
+    end
+
+    context "automated" do
+      it { expect(obj.channel).to eq 'test_client' }
+    end
+  end
+
+  describe "#config_filename" do
+    context "parameter provided" do
+      it { expect(obj.config_filename("whatever.yml")).to eq "whatever.yml" }
+    end
+
+    context "automated" do
+      it { expect(obj.config_filename).to eq "test_client.yml" }
+    end
+  end
+
+  describe "#config_path" do
+    context "parameter provided" do
+      it { expect(obj.config_path("config/hdsq/")).to eq "config/hdsq/" }
+    end
+
+    context "automated" do
+      it { expect(obj.config_path).to eq "./config/" }
+    end
+  end
+
+  describe "#config_file_path" do
+    it { expect(obj.config_file_path).to eq "./config/test_client.yml"  }
+  end
+
+  describe "#read_options" do
+    context "no config file" do
+      it { expect(obj.read_opts).to eq obj.default_opts }
+    end
+  end
+
+  context "file present" do
+    context "no config file" do
+      it { expect(obj.read_opts('./spec/shared/minimum_config.yml')).to eq ({threaded: true, timeout: 10}) }
+    end
+  end
+
+end
