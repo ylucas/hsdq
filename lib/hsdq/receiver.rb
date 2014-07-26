976bac0b491f406841bdd50aa815c7199ece8f55
Yves Lucas <ylucas@ylucas.com>
2014-07-25 19:30:02 -0700

    yard receiver

--- lib/hsdq/receiver.rb
+++ lib/hsdq/receiver.rb
@@ -1,15 +1,35 @@
 
-
 module Hsdq
+  # This module process the incoming messages and call one of 5 messages (ack, request, callback, feedback or error)
+  # depending on the type of the message.
   module Receiver
 
-    # Placeholder methods for the message types
+
+    # **Placeholder for request received. You must override hsdq_request in your HsdqXxx class**<br>
+    # After this method has run, there will be no further processing.
+    #
+    # @param [Hash] message, the message to process
+    # @param [Hash] context, the request that was originally sent.
+    # @return [String] placeholder string message, will be what your method will be returning.
     def hsdq_request(message, context);  placeholder; end
+    # **Placeholder for ack received. You must override hsdq_request in your HsdqXxx class**
+    # @see #hsdq_request
     def hsdq_ack(message, context);      placeholder; end
+    # **Placeholder for callback received. You must override hsdq_request in your HsdqXxx class**
+    # @see #hsdq_request
     def hsdq_callback(message, context); placeholder; end
+    # **Placeholder for feedback received. You must override hsdq_request in your HsdqXxx class**
+    # @see #hsdq_request
     def hsdq_feedback(message, context); placeholder; end
+    # **Placeholder for error received. You must override hsdq_request in your HsdqXxx class**
+    # @see #hsdq_request
     def hsdq_error(message, context);    placeholder; end
 
+    # Send the ACk and start the processing for the message just received.<br>
+    # The processing will be either executed synchronously or a new thread will be started based on the configuration.
+    #
+    # @param [Json string] spark (ephemeral) just popped out of the queue in json format
+    # @param [Hash] HsdqXxx class configuration options
     def hsdq_ignit(raw_spark, options)
       spark = h_spark raw_spark
       send_ack spark
@@ -28,11 +48,21 @@ module Hsdq
       raw_spark.kind_of?(Array) ? raw_spark.last : raw_spark
     end
 
+    # return the spark (ephemeral part of the message) from the message list
+    # @param [Json string or Array] from the list
+    # @return [Hash] spark ready to be used by the system
     def h_spark(raw_spark)
       JSON.parse get_spark(raw_spark), {symbolize_names: true}
     end
 
-    # Entry point for the task to process
+    # Entry point for the task to process, this is what is executed in the threads when a message is pulled.
+    # - Pull the burst (line with the request or response) from the the hash<br>
+    # - Pull the context related to a response if it exist
+    # - Set values for the next hop context in case of a request.
+    # - Call one of the 5 methods (request, ack, callback, feedback, error) in your hsdqXxx class (or the placeholder)
+    # based on the message type
+    # @param [Hash] spark
+    # @param [Hash] hsdq class options from setup
     def sparkle(spark, options)
       puts spark.inspect
       burst, ctx_burst = get_burst spark, options
@@ -52,9 +82,10 @@ module Hsdq
 
           hsdq_request burst, context
       end
-
     end
 
+    # Save for future use context data into the thread_store
+    # @param [Hash] spark
     def set_context(spark)
       # store in thread_store for later use
       sent_to spark[:sender]
@@ -62,6 +93,11 @@ module Hsdq
       context_params({ reply_to: spark[:previous_sender], spark_uid: spark[:spark_uid]})
     end
 
+    # Manage pulling:
+    # - the burst (persistent action) associated with the spark from the matching Redis hash
+    # - if needed the context data
+    # @param [Hash] spark
+    # @param [Hash] options for the app
     def get_burst(spark, _options={})
       # get the context parameters
       context_h = spark[:context]
@@ -84,7 +120,10 @@ module Hsdq
       [burst, burst_context]
     end
 
-    # return an array [burst, context]
+    # Execute a multi transaction to get the burst and the context from Redis in a single call
+    # @param [Proc] burst_p query to pull the burst from redis
+    # @param [Proc] context_p query to pull the context from redis
+    # @return [array] [burst, context]
     def pull_burst(burst_p, burst_context_p)
       cx_data.multi do
         burst_p.call
@@ -92,10 +131,20 @@ module Hsdq
       end
     end
 
+    # If there is no context this method is used instead of pull_burst
+    # @see #pull_burst
+    # @param [Proc] burst_p query to pull the burst from redis
     def pull_burst_only(burst_p)
       [burst_p.call, nil]
     end
 
+    # Spark validation, call valid_type?. If invalid:
+    # - an error is sent back to the sender
+    # - false is returned to the processing to stop the action.
+    # @param [Hash] spark
+    # @param [Hash] options Application options
+    # @return [Boolean] true in case of valid spark,
+    # @return [Hash] the error message if an error is raised
     def validate_spark(spark, options)
       begin
         raise ArgumentError.new("Illegal type #{spark[:type]}") unless valid_type? spark[:type]
@@ -106,6 +155,11 @@ module Hsdq
 
     end
 
+    # Call whitelisted? to verify the the topic and task are legit.
+    # @param [Hash] spark
+    # @param [Hash] options
+    # @return [Boolean] if whitelist validation is successful
+    # @return [Hash] the error message if an error is raised
     def check_whitelist(spark, options)
       begin
         raise ArgumentError.new("Illegal argument in topic or task") unless whitelisted? spark, options
@@ -114,10 +168,15 @@ module Hsdq
       end
     end
 
+    # validate the topic and the task
     def whitelisted?(spark, options)
       valid_topic?(spark, options) && valid_task?(spark, options)
     end
 
+    # Send an error message back to the sender
+    # @param [Hash] Spark
+    # @param [ArgumentError] if invalid
+    # @return [Hash] the error message
     def reject_spark(spark, e)
       error = {
         sent_to: spark[:sender],
@@ -131,20 +190,28 @@ module Hsdq
       error
     end
 
+    # Send the ack back to the sender in case of a request
     def send_ack(spark)
       return unless ['request', :request].include? spark[:type]
       ack_msg = spark.merge sent_to: spark[:sender], sender: channel
       hsdq_send_ack ack_msg
     end
 
+    # Hash of the internal authorized message types
     def hsdq_authorized_types
       [:request, :ack, :feedback, :callback, :error]
     end
 
+    # Cached value of the tasks authorized to be processed
+    # @param [Array] Additional tasks to the one setup in the configuration file
+    # @return [Array] the authoriced tasks
     def hsdq_authorized_tasks(*tasks)
       @hsdq_authorized_tasks ||= [hsdq_opts[:tasks], [tasks]].flatten
     end
 
+    # Cached value of the topics authorized to be processed
+    # @param [Array] Additional tasks to the one setup in the configuration file
+    # @return [Array] the authoriced topics
     def hsdq_authorized_topics(*topics)
       @hsdq_authorized_topics ||= [hsdq_opts[:topics], [topics]].flatten
     end
@@ -159,6 +226,7 @@ module Hsdq
       hsdq_authorized_topics.include?(spark[:topic].to_sym)
     end
 
+    # @return [boolean] true if the message received is a response
     def response?(spark)
       [:callback, :feedback, :error].include? spark[:type].to_sym
     end
