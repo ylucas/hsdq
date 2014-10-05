1a048eda2270386c57a0164d9e8e22572e099f6d
Yves Lucas <ylucas@ylucas.com>
2014-10-04 22:39:46 -0700

    yard shared

--- lib/hsdq/shared.rb
+++ lib/hsdq/shared.rb
@@ -10,16 +10,28 @@ module Hsdq
       [:request, :ack, :callback, :feedback, :error].include? type.to_sym
     end
 
+    # Build the namespaced key for the main hash storing the message history (collection of 'burst')
+    # @param [Hash] message_or_spark a burst or a spark
+    # @return [String] the unique key for the main redis hash
+    # @return nil if message_or_spark or the uid is nil
     def hsdq_key(message_or_spark)
       return unless message_or_spark
       "hsdq_h_#{message_or_spark[:uid]}" if message_or_spark[:uid]
     end
 
+    # Build the namespaced key for the spark and burst unique shared uid
+    # @param [Hash] spark
+    # @return [String] the unique namespaced key
+    # @return nil if spark or the spark_uid is nil
     def burst_key(spark)
-      return unless spark
+      return unless spark &&
       "#{spark[:type]}_#{spark[:spark_uid]}" if spark[:type] && spark[:spark_uid]
     end
 
+    # Build the namespaced key for the main session hash
+    # @param [string] session_id the unique uid for the session
+    # @return [String] the unique namespaced key
+    # @return nil if session_id
     def session_key(session_id)
       return unless session_id
       "hsdq_s_#{session_id}"
