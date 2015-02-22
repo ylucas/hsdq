d2ff1886e1f9ff2316ae91a46bd238647eec6539
Yves Lucas <ylucas@ylucas.com>
2015-02-21 23:33:40 -0800

    admin still wip

---
+++ lib/hsdq/admin.rb
@@ -0,0 +1,62 @@
+module Hsdq
+  module Admin
+
+    def listener_id(listener_id=nil)
+      @listener_id = listener_id if listener_id
+      @listener_id
+    end
+
+    def listener_version(listener_version=nil)
+      @listener_version = listener_version if listener_version
+      @listener_version
+    end
+
+    def admin_channel
+      "#{environment}_#{channel}_admin"
+    end
+
+    def admin_versionned_channel
+      "#{admin_channel}__#{listener_version}"
+    end
+
+    def admin_id_channel
+      "#{admin_channel}__#{listener_id}"
+    end
+
+    def admin_channels
+      @admin_channels ||= %w(admin )
+    end
+
+    def admin_listener
+      begin
+        p "starting admin channels #{admin_channel}, #{admin_versionned_channel}"
+        cx_admin.subscribe(admin_channel, admin_versionned_channel) do |on|
+          on.message do |a_channel, admin_message_j|
+            p "received admin message: #{admin_message_j} from admin channel #{admin_channel} "
+            process_admin_message(admin_channel, admin_message_j)
+          end
+        end
+      rescue Redis::BaseConnectionError => e
+        p e.inspect
+        sleep(1)
+        retry
+      end
+    end
+
+    def process_admin_message(_admin_channel, admin_message_j)
+      admin_message = JSON.parse(admin_message_j) rescue {'params' => {'task' => ""}}
+      task = admin_message['params']['task']
+      case task
+        when 'stop'
+          hsdq_stop!
+        when 'start'
+          hsdq_start!
+        when 'exit'
+          hsdq_exit!
+        when 'kill'
+          # hsdq_kill! # here kill all the threads and exit
+      end
+    end
+
+  end
+end
