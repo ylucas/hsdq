4d414271fd7e3cc55e810af1dadc136a93c29b78
Yves Lucas <ylucas@ylucas.com>
2016-11-06 19:58:09 -0800

    docs

--- lib/hsdq/connectors.rb
+++ lib/hsdq/connectors.rb
@@ -4,8 +4,8 @@ module Hsdq
 
   # This module contains the logic for different connection to the redis layer.
   #
-  # They can be connected to the same Redis instance, but it is recommended to use different database connections in order to
-  # segregate the different usages.
+  # They can be connected to the same Redis instance, but it is recommended to use different
+  # database connections in order to segregate the different usages.
   #
   # In production of large applications you should use different instances for the different layers.
   # The configuration files are named after your class name hsdq_yourclass.yml
@@ -19,7 +19,8 @@ module Hsdq
     end
 
     # Establish an unblocked connection for the sender and also pulling data from Redis
-    # @return [Redis connection] This connection is used to send messages as well as to retrieve data from the message hash
+    # @return [Redis connection] This connection is used to send messages as well as to retrieve
+    #                            data from the message hash
     def cx_data
       @cx_data ||= Redis.new cx_opts[:message]
     end
